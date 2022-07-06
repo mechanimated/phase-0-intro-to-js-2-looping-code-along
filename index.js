26de7900b9c871483c09024cc2b4be03b9631665
Liz Boika <mdaicdxl@gmail.com>
2022-07-05 22:00:59 -0400

    commit tree dbce86516e37725caa3c6740def5d9a7854dc0bd

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const gift = "surprise";
+
+function writeCards(names, gift) {
+
+let tmpnames = new Array();
+
+  for (let i = 0; i < names.length; i++) {
+    tmpnames[i] = `Thank you, ${names[i]}, for the wonderful ${gift} gift!`;
+  }
+    return tmpnames;
+}
+
+
+
+function countDown() {
+let countdown = 10;
+
+for (let countdown = 10; countdown >= 0 ; countdown--) {
+  console.log(countdown);
+}
+
+}
